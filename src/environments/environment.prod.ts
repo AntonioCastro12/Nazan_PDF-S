175c90d507c3b70ed2d9bd4596d297c183a35352
Rainer Ramirez <ironrai@gmail.com>
2023-11-16 08:36:33 -0600

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   hostname: 'localhost',
-  host: 'https://preprod-front.impuls.com.mx',
-  apiUrl: 'https://preprod-api.impuls.com.mx',
+  host: 'https://administrativo.impuls.com.mx',
+  apiUrl: 'https://administrativoapi.impuls.com.mx',
   utilityUrl: 'https://utilityapi.impuls.com.mx',
   dashboardUrl: 'https://dashboard.impuls.com.mx',
   hydraUrl: 'https://hydra.impuls.com.mx',
