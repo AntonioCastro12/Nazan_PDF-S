131adc3b9c17cbea7481ee6d3b7bb3130614f47f
Leonel Camero <leonel.camero@gmail.com>
2024-05-08 12:05:03 -0400

    fix: "version": "2.0.2"

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,16 +1,16 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-  "dirty": true,
-  "raw": "0d4011d-dirty",
-  "hash": "0d4011d",
-  "distance": null,
-  "tag": null,
-  "semver": null,
-  "suffix": "0d4011d-dirty",
-  "semverString": null,
-  "version": "2.0.1",
-  "date": "2023-04-27T14:01:22.174Z",
-  "shortdate": "2023-04-27"
+    "dirty": true,
+    "raw": "5e60a9f-dirty",
+    "hash": "5e60a9f",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "5e60a9f-dirty",
+    "semverString": null,
+    "version": "2.0.2",
+    "date": "2024-05-08T16:03:28.114Z",
+    "shortdate": "2024-05-08"
 };
 /* tslint:enable */
