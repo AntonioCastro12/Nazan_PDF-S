c8bcaa5925a8fe51fca663067477753b36570e18
Alfredo Cano <alfredo.cano@impuls.mx>
2024-07-15 10:25:19 -0600

    structure of pdf-preciado-form changed at ts file pdw writer working propertly

--- src/app/layout/reports/pdf-preciado/pages/pdf-preciado-form/pdf-preciado-form.component.ts
+++ src/app/layout/reports/pdf-preciado/pages/pdf-preciado-form/pdf-preciado-form.component.ts
@@ -36,157 +36,6 @@ interface ExcelData {
 })
 export class PdfPreciadoFormComponent {
 
-
- //variable para accesar a los datos del excel ID-codigoInternet   PRICE-Precio
- rows: ExcelData[] = [ { ID: "Find", PRICE: "Find3" }];
-
-
- onFileExcel(event: Event) :void{
-  const target = event.target as HTMLInputElement;
-  if (target.files && target.files.length > 0) {
-      if(target.files[0].name.substring(target.files[0].name.lastIndexOf('.'),target.files[0].name.length) != ".xlsx")
-        return;
-      //this.excel = target.files[0].name;
-      this._taGralStateService.state.excel = target.files[0];
-  }
-  else{
-    console.log("Vacio");
-  }
-}
-
-onProcessingExcel(event: Event):void{
-  if(this._taGralStateService.state.excel == null){
-    alert("EXCEL VACIO");
-    return;
-  }
-  this.ReadExcel();
-}
-
-async ReadExcel(){
-  console.log("Reading excel file")
-  if(this._taGralStateService.state.excel != null){
-    const arrayBuffer = await this._taGralStateService.state.excel.arrayBuffer();
-    var workbook = XLSX.read(arrayBuffer);
-    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    const lstData = XLSX.utils.sheet_to_json<ExcelData>(worksheet);
-    this.rows=lstData;
-  }
-  console.log("rows: ", this.rows)
-}
-//******************************************* */
-
-
-// ********************* PDF
-onFilePdf(event: Event) :void{
-  const target = event.target as HTMLInputElement;
-  if (target.files && target.files.length > 0) {
-      if(target.files[0].name.substring(target.files[0].name.lastIndexOf('.'),target.files[0].name.length) != ".pdf")
-        return;
-      this._taGralStateService.state.pdf = target.files[0];
-  }
-  else{
-    console.log("Vacio");
-  }
-}
-
-onProcessingPDF(event: Event):void{
-  if(this._taGralStateService.state.pdf == null){
-    alert("PDF VACIO");
-    return;
-  }
-  this.UpdatePDF();
-}
-
-async UpdatePDF(){
-  console.log("updating pdf file")
-if(this._taGralStateService.state.pdf != null){
-
-
-    const arrayBuffer = await this._taGralStateService.state.pdf.arrayBuffer();
-    const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise;
-    const pdfDoc = await PDFDocument.load(arrayBuffer);
-
-    for(let f=0; f < this.rows.length; f++)
-    {
-        console.log("codigoInternet: " + this.rows[f].ID + " - Precio: " + this.rows[f].PRICE);
-        const searchWord = this.rows[f].ID.toString();
-        const numberToAdd = this.rows[f].PRICE.toString();
-
-        for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
-          const page = await pdf.getPage(pageNum);
-          const textContent = await page.getTextContent();
-          //console.log(textContent);
-          const items = textContent.items.map(item => item as any);
-          //const pageText = items.map(item => item.str).join(' ');
-          //console.log(pageText);
-          for (let i = 0; i < items.length; i++) {
-            if (items[i].str.includes(searchWord)) {
-              //const { x,f,z, y, width, height } = items[i].transform;
-              const x=items[i].transform[0];
-              const y=items[i].transform[3];
-              const width=items[i].transform[4];
-              const height=items[i].transform[5];
-              //console.log(items[i].transform);
-              const pdfPage = pdfDoc.getPage(pageNum - 1);
-
-              pdfPage.drawRectangle({
-                x: width +140,
-                y: height -5, // ajusta la posición del fondo
-                width: 40,
-                height: 20,
-                color: rgb(0.9, 0.9, 0.9)
-              });
-
-              pdfPage.drawText(numberToAdd, {
-                x: width +150,
-                y: height + 0, // Ajusta esta posición según necesites
-                maxWidth:width,
-                lineHeight:height,
-                size: 12,
-                color: rgb(0, 0, 0),
-              });
-            }
-          }
-        }
-    }
-    const modifiedPdfBytes = await pdfDoc.save();
-    const blob = new Blob([modifiedPdfBytes], { type: 'application/pdf' });
-    this._taGralStateService.state.modifiedPdfUrl = URL.createObjectURL(blob);
-  }
-  console.log("Pdf url ")
-}
-//******************************************* */
-// ********************* PROCESAR DATA
-onProcessing(event: Event):void{
-
-  if(this._taGralStateService.state.excel == null){
-    alert("EXCEL VACIO");
-    return;
-  }
-
-  if(this._taGralStateService.state.pdf == null){
-    alert("PDF VACIO");
-    return;
-  }
-
-  console.log("Procesando Datos");
-  this.ProccessData();
-}
-
-ProccessData(){
-  this.ReadExcel();
-  this.UpdatePDF();
-
-}
-//******************************************* */
-
-
-
-
-
-
-
-
   /**
    * datos para funcionamiento de lectura de excel y pdf
    */
@@ -229,6 +78,151 @@ ProccessData(){
     this.onFillForm();
   }
 
+
+
+  //variable para accesar a los datos del excel ID-codigoInternet   PRICE-Precio
+  rows: ExcelData[] = [{ ID: "Find", PRICE: "Find3" }];
+
+
+  onFileExcel(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    if (target.files && target.files.length > 0) {
+      if (target.files[0].name.substring(target.files[0].name.lastIndexOf('.'), target.files[0].name.length) != ".xlsx")
+        return;
+      //this.excel = target.files[0].name;
+      this._taGralStateService.state.excel = target.files[0];
+    }
+    else {
+      console.log("Vacio");
+    }
+  }
+
+  onProcessingExcel(event: Event): void {
+    if (this._taGralStateService.state.excel == null) {
+      alert("EXCEL VACIO");
+      return;
+    }
+    this.ReadExcel();
+  }
+
+  async ReadExcel() {
+    console.log("Reading excel file")
+    if (this._taGralStateService.state.excel != null) {
+      const arrayBuffer = await this._taGralStateService.state.excel.arrayBuffer();
+      var workbook = XLSX.read(arrayBuffer);
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      const lstData = XLSX.utils.sheet_to_json<ExcelData>(worksheet);
+      this.rows = lstData;
+    }
+    console.log("rows: ", this.rows)
+  }
+  //******************************************* */
+
+
+  // ********************* PDF
+  onFilePdf(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    if (target.files && target.files.length > 0) {
+      if (target.files[0].name.substring(target.files[0].name.lastIndexOf('.'), target.files[0].name.length) != ".pdf")
+        return;
+      this._taGralStateService.state.pdf = target.files[0];
+    }
+    else {
+      console.log("Vacio");
+    }
+  }
+
+  onProcessingPDF(event: Event): void {
+    if (this._taGralStateService.state.pdf == null) {
+      alert("PDF VACIO");
+      return;
+    }
+    this.UpdatePDF();
+  }
+
+  async UpdatePDF() {
+    console.log("updating pdf file")
+    if (this._taGralStateService.state.pdf != null) {
+
+
+      const arrayBuffer = await this._taGralStateService.state.pdf.arrayBuffer();
+      const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise;
+      const pdfDoc = await PDFDocument.load(arrayBuffer);
+
+      for (let f = 0; f < this.rows.length; f++) {
+        console.log("codigoInternet: " + this.rows[f].ID + " - Precio: " + this.rows[f].PRICE);
+        const searchWord = this.rows[f].ID.toString();
+        const numberToAdd = `$ ${this.rows[f].PRICE.toString()}`;
+
+        for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+          const page = await pdf.getPage(pageNum);
+          const textContent = await page.getTextContent();
+          //console.log(textContent);
+          const items = textContent.items.map(item => item as any);
+          //const pageText = items.map(item => item.str).join(' ');
+          //console.log(pageText);
+          for (let i = 0; i < items.length; i++) {
+            if (items[i].str.includes(searchWord)) {
+              //const { x,f,z, y, width, height } = items[i].transform;
+              const x = items[i].transform[0];
+              const y = items[i].transform[3];
+              const width = items[i].transform[4];
+              const height = items[i].transform[5];
+              //console.log(items[i].transform);
+              const pdfPage = pdfDoc.getPage(pageNum - 1);
+
+              pdfPage.drawRectangle({
+                x: width - 0,
+                y: height - 40, // ajusta la posición del fondo
+                width: 50,
+                height: 20,
+                color: rgb(0.9, 0.9, 0.9),
+                opacity: 0.4,
+              });
+
+              pdfPage.drawText(numberToAdd, {
+                x: width + 8,
+                y: height - 35, // Ajusta esta posición según necesites
+                maxWidth: width,
+                lineHeight: height,
+                size: 12,
+                color: rgb(0, 0, 0),
+              });
+            }
+          }
+        }
+      }
+      const modifiedPdfBytes = await pdfDoc.save();
+      const blob = new Blob([modifiedPdfBytes], { type: 'application/pdf' });
+      this._taGralStateService.state.modifiedPdfUrl = URL.createObjectURL(blob);
+    }
+    console.log("Pdf url ")
+  }
+  //******************************************* */
+  // ********************* PROCESAR DATA
+  onProcessing(event: Event): void {
+
+    if (this._taGralStateService.state.excel == null) {
+      alert("EXCEL VACIO");
+      return;
+    }
+
+    if (this._taGralStateService.state.pdf == null) {
+      alert("PDF VACIO");
+      return;
+    }
+
+    console.log("Procesando Datos");
+    this.ProccessData();
+  }
+
+  ProccessData() {
+    this.ReadExcel();
+    this.UpdatePDF();
+
+  }
+
+  
   onFillForm() {
     this._taGralStateService.state.form = this._formBuilder.group({
       startDate: [this.today, [Validators.required], []],
